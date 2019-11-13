b2394a72153eea05b9580ac80671b59c6c064074
Mark Gowen <mark.gowen78@gmail.com>
2019-11-13 10:22:57 -0800

    guided commit

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,28 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  // see if there is a token
+  // check validity => rehash the header + payload + secret to see if matches signature
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log('failed verification', err);
+        res.status(401).json({ message: 'not verified'})
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({
+      message: 'no token provided'
+    })
   }
+
 };
